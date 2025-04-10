8ab2c1edc88c949e9a50fbbf8b77f254eac468fe
Scheltema <thomas.scheltema@hp.com>
2025-04-10 18:15:02 -0500

    create more routes and project outline

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,6 +1,16 @@
-import { Routes } from '@angular/router';
-import { HealthCheckComponent } from './health-check/health-check.component';
+import { Routes } from "@angular/router";
+import { HealthCheckComponent } from "./health-check/health-check.component";
+import { HomeComponent } from "./home/home.component";
+import { NotFoundComponent } from "./not-found/not-found.component";
 
 export const routes: Routes = [
-  { path: 'health-check', component: HealthCheckComponent },
+  {
+    path: "health-check",
+    component: HealthCheckComponent,
+    title: "Health Check",
+  },
+  { path: "", component: HomeComponent, title: "Home" }, // doesn't need "/"
+
+  // Redirect for unmatched paths
+  { path: "**", component: NotFoundComponent, title: "Page Not Found" },
 ];
